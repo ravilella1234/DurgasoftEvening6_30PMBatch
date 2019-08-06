4176f6a77c38def49eed9eca647446ec784ac5c3
ravilella1234 <ravilella1234>
2019-08-06 19:39:01 +0530

    firefox Profiling

--- src/com/selenium/BaseTest.java
+++ src/com/selenium/BaseTest.java
@@ -6,8 +6,14 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeDriverService;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.ie.InternetExplorerDriverService;
 
 public class BaseTest 
 {
@@ -27,13 +33,41 @@ public class BaseTest
 	
 	public static void launch(String browser)
 	{
-		if(dataload.get(browser).equals("chrome")) {
+		if(dataload.get(browser).equals("chrome")) 
+		{
+			//System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\Drivers\\chromedriver.exe");
+			//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//drivers//chromedriver.exe");
+			System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY, System.getProperty("user.dir")+"//drivers//chromedriver.exe");
 			driver=new ChromeDriver();
-		}else if(dataload.get(browser).equals("firefox")) {
-			driver=new FirefoxDriver();
-		}else if(dataload.get(browser).equals("edge")) {
+		}
+		else if(dataload.get(browser).equals("firefox")) 
+		{
+			//drivers initialization
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"//drivers//geckodriver.exe");
+			
+			//logs removing in console
+			System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "C:\\Users\\DELL\\Desktop\\Drivers\\logs\\ff.txt");
+			
+			FirefoxOptions option=new FirefoxOptions();
+			
+			ProfilesIni p=new ProfilesIni();
+			FirefoxProfile profile = p.getProfile("p1");
+			
+			//notifications
+			profile.setPreference("dom.webnotifications.enabled", false);
+			
+			option.setProfile(profile);
+			
+			//Opening Firefox Browser
+			driver=new FirefoxDriver(option);
+		}
+		else if(dataload.get(browser).equals("edge")) 
+		{
+			System.setProperty(EdgeDriverService.EDGE_DRIVER_EXE_PROPERTY, System.getProperty("user.dir")+"//drivers//MicrosoftWebDriver.exe");
 			driver=new EdgeDriver();
-		}else {
+		}
+		else 
+		{
 			System.out.println("Given browser is not matched.....");
 		}
 		
@@ -44,6 +78,7 @@ public class BaseTest
 	{
 		//driver.get(dataload.getProperty(url));
 		driver.navigate().to(dataload.getProperty(url));
+		driver.manage().window().maximize();
 	}
 
 }
--- src/com/selenium/TC_002.java
+++ src/com/selenium/TC_002.java
@@ -3,11 +3,13 @@ package com.selenium;
 public class TC_002 extends BaseTest
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception 
 	{
-		launch("chrome");
+		init();
 		
-		navigate("https://amazon.in");
+		launch("firefoxbrowser");
+		
+		navigate("iciciurl");
 
 	}
 
