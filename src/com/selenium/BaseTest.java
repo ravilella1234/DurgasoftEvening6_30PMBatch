055810370cc81d2418c4aa24e7ef318f21e6e66a
ravilella1234 <ravilella1234>
2019-08-07 19:21:20 +0530

    Chrome ,IE, Edge options classes

--- src/com/selenium/BaseTest.java
+++ src/com/selenium/BaseTest.java
@@ -1,19 +1,24 @@
 package com.selenium;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.Properties;
 
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeDriverService;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerDriverService;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BaseTest 
 {
@@ -35,10 +40,27 @@ public class BaseTest
 	{
 		if(dataload.get(browser).equals("chrome")) 
 		{
+			
+			//drivers initialization
 			//System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\Drivers\\chromedriver.exe");
 			//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//drivers//chromedriver.exe");
 			System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY, System.getProperty("user.dir")+"//drivers//chromedriver.exe");
-			driver=new ChromeDriver();
+			
+			//Logs clearing
+			System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "null");
+			
+			//Chrome Profile creation
+			ChromeOptions option=new ChromeOptions();
+			option.addArguments("user-data-dir=C:\\Users\\DELL\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 6");
+			
+			option.addArguments("--disable-notifications");
+			option.addArguments("--disable-infobars");
+			option.addArguments("--start-maximized");
+			
+			//Proxy IP Configuration
+			//option.addArguments("--proxy-server=http://192.168.90.84:1234");
+			
+			driver=new ChromeDriver(option);
 		}
 		else if(dataload.get(browser).equals("firefox")) 
 		{
@@ -56,6 +78,12 @@ public class BaseTest
 			//notifications
 			profile.setPreference("dom.webnotifications.enabled", false);
 			
+			
+			//proxy servers
+			/*profile.setPreference("network.proxy.type", 1);
+			profile.setPreference("network.proxy.socks", "192.168.90.54");
+			profile.setPreference("network.proxy.socks_port", 1744);*/
+			
 			option.setProfile(profile);
 			
 			//Opening Firefox Browser
@@ -64,7 +92,31 @@ public class BaseTest
 		else if(dataload.get(browser).equals("edge")) 
 		{
 			System.setProperty(EdgeDriverService.EDGE_DRIVER_EXE_PROPERTY, System.getProperty("user.dir")+"//drivers//MicrosoftWebDriver.exe");
+			
+			//logs
+			System.setProperty(EdgeDriverService.EDGE_DRIVER_EXE_PROPERTY, "null");
+			
 			driver=new EdgeDriver();
+		}
+		else if(browser.equalsIgnoreCase("ie")) 
+		{
+			//logs
+			System.setProperty(InternetExplorerDriverService.IE_DRIVER_LOGFILE_PROPERTY, "D:\\IE.log");
+			
+			InternetExplorerOptions option=new InternetExplorerOptions();
+			
+			//proxy servers
+			DesiredCapabilities cap=new DesiredCapabilities();
+			
+			String proxy="80.200.90.81:4444";
+			Proxy p =new Proxy();
+			p.setAutodetect(false);
+			p.setProxyType(p.getProxyType());
+			p.setSocksProxy(proxy);
+			cap.setCapability(CapabilityType.PROXY, p);
+			option.merge(cap);
+			
+			driver=new InternetExplorerDriver(option);
 		}
 		else 
 		{
@@ -78,7 +130,7 @@ public class BaseTest
 	{
 		//driver.get(dataload.getProperty(url));
 		driver.navigate().to(dataload.getProperty(url));
-		driver.manage().window().maximize();
+		//driver.manage().window().maximize();
 	}
 
 }
--- src/com/selenium/TC_002.java
+++ src/com/selenium/TC_002.java
@@ -7,7 +7,7 @@ public class TC_002 extends BaseTest
 	{
 		init();
 		
-		launch("firefoxbrowser");
+		launch("chromebrowser");
 		
 		navigate("iciciurl");
 
